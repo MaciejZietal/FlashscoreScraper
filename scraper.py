63fd2a6452e7b4246b5275a53c51a92d07dafe6d
MaciejZietal <maciejzietal@wp.pl>
2024-02-12 21:44:56 +0100

    feat: added reading stats from all matches in get_all_matches method

--- scraper.py
+++ scraper.py
@@ -1,11 +1,15 @@
 import time
+import random
+
+import pandas as pd
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import StaleElementReferenceException, NoSuchElementException
 
 from utils import create_driver, get_proxies
+from match import Match
 
 PROXIES  = get_proxies()
 
@@ -14,11 +18,31 @@ class Scraper():
         self.country = country
         self.division = division
         self.season = season
+        self.matches_stats = []
+        self.matches_with_errors = []
         
-    def get_all_results(self):
+    def get_all_matches(self):
         url = self.generate_league_url()
         id_list = self.get_all_matches_ids(url)
+        random.shuffle(id_list)
+        driver = create_driver(PROXIES)
+        for id in random.choices(id_list, k=20):
+            single_match = Match(id, driver)
+            try:
+                single_match.generate_match_info()
+            except:
+                self.matches_with_errors.append(id)
+            self.matches_stats.append(single_match.get_match_info())
+            
+            # change driver if generated number is greater than 0.75
+            if random.random() > 0.75:
+                driver.quit()
+                driver = create_driver(PROXIES)
+                time.sleep(1)
+                
+        driver.quit()
         
+        return pd.concat(self.matches_stats)
         
     def generate_league_url(self) -> str:
         """Generate link to league results.
@@ -59,5 +83,7 @@ class Scraper():
         for id in WebDriverWait(driver, 30).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '[id^=g_1_]'))):
             single_id = id.get_attribute("id").strip("g_1_")
             id_list.append(single_id)
+            
+        driver.quit()
 
         return id_list
